5b36eec021e555b6299e933a748ede6a36f64c70
LaPlusGrande <2609lisa@gmail.com>
2022-03-11 20:02:26 +0100

    Physio Workout Kotlin

---
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/MainActivity.kt
@@ -0,0 +1,43 @@
+package de.fhkiel.srcms.apps.therapy.physical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+
+import com.aldebaran.qi.sdk.QiSDK
+
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+
+import com.aldebaran.qi.sdk.builder.SayBuilder
+
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
+
+import com.aldebaran.qi.sdk.`object`.conversation.Say
+
+
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this)
+    }
+
+    override fun onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext) {
+        // The robot focus is gained.
+    }
+
+    override fun onRobotFocusLost() {
+        // The robot focus is lost.
+    }
+
+    override fun onRobotFocusRefused(reason: String) {
+        // The robot focus is refused.
+    }
+}
