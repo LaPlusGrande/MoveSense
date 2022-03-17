ca0e91365715f3f1867ef818145f761c8bc1cf56
Hannes Eilers <mail@hanneseilers.de>
2022-03-17 12:12:21 +0100

    Updated renaming packages and deleted build

--- app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/database/UserRVAdapter.java
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/database/UserRVAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import de.fhkiel.srcms.apps.therapy.physical.R;
+import de.fhkiel.srcms.apps.therapy.physical.p.workout.R;
 
 import java.util.ArrayList;
 
