e2f83554b20af49994bcfa3f08fb9b5af98625db
Hannes Eilers <mail@hanneseilers.de>
2022-06-15 18:38:00 +0200

    Made HttpClient to store login key and created constructor to use preset login key.

--- app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
@@ -17,11 +17,17 @@ public class HttpClient
 
     private static String TAG = HttpClient.class.getName();
     private PepperAnimation pepperAnimation = new PepperAnimation();
-    private static String key;
+    private String key = "";
     private String physioKey = "{'key': '47654d4cfcefe1092b7afb12d18b55c9859d2bd9226e381c4f50a0447d1122afe84dd3cb94c8aa00fd09ee8a5847977efa5a7e4c4aa16f0754f9493923d96cef8b9ce21334700482b7a8b7d68ae852a0f4133a48b8fa3084c034978374a99169eedde1216d04eda810ed981b5cb946ff310fdf5770b581cde5951546adb238d7'}";
 
     // Raspberry Pi, IP 192.168.178.10, Port 5837
     private static String loggingServerIP = "192.168.1.7";
+
+    public HttpClient(){}
+
+    public HttpClient(String key) {
+        this.key = key;
+    }
 
 
     public String login (){
