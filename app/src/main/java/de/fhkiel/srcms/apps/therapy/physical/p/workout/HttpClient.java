48775fb92cc671058d758848036990e118cf7f48
Hannes Eilers <mail@hanneseilers.de>
2022-06-15 18:23:43 +0200

    Optimized code

--- app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
@@ -20,6 +20,7 @@ public class HttpClient
     private static String key;
     private String physioKey = "{'key': '47654d4cfcefe1092b7afb12d18b55c9859d2bd9226e381c4f50a0447d1122afe84dd3cb94c8aa00fd09ee8a5847977efa5a7e4c4aa16f0754f9493923d96cef8b9ce21334700482b7a8b7d68ae852a0f4133a48b8fa3084c034978374a99169eedde1216d04eda810ed981b5cb946ff310fdf5770b581cde5951546adb238d7'}";
 
+    // Raspberry Pi, IP 192.168.178.10, Port 5837
     private static String loggingServerIP = "192.168.1.7";
 
 
@@ -41,9 +42,6 @@ public class HttpClient
             }
 
             try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-//                while ((key = br.readLine()) != null) {
-//                    System.out.println(key);
-//                }
                 key = br.readLine();
                     System.out.println("key" + key);
             }catch (IOException e){
@@ -58,10 +56,7 @@ public class HttpClient
 
         URLConnection connection;
         try {
-//            URL url = new URL("http://10.0.2.2:5837/app/Physio-Workout/log/app");
-//          Raspberry Pi, IP 192.168.178.10, Port 5837
-
-            URL url = new URL("http://192.168.178.10:5837/app/Physio-Workout/log/app");
+            URL url = new URL("http://" + loggingServerIP + ":5837/app/Physio-Workout/log/app");
 
             connection = url.openConnection();
             connection.setDoOutput(true);
