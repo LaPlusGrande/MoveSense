88fc6170cb7a2aa1fe889da13ead43dfd98443ce
Hannes Eilers <mail@hanneseilers.de>
2022-06-15 20:59:41 +0200

    Updated log messages and object to string conversion to fit json style string data.

--- app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
@@ -2,6 +2,9 @@ package de.fhkiel.srcms.apps.therapy.physical.p.workout;
 
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -38,6 +41,7 @@ public class HttpClient
             String physioKey = "{'key': '47654d4cfcefe1092b7afb12d18b55c9859d2bd9226e381c4f50a0447d1122afe84dd3cb94c8aa00fd09ee8a5847977efa5a7e4c4aa16f0754f9493923d96cef8b9ce21334700482b7a8b7d68ae852a0f4133a48b8fa3084c034978374a99169eedde1216d04eda810ed981b5cb946ff310fdf5770b581cde5951546adb238d7'}";
             connection.setRequestProperty("Content-Length", physioKey);
 
+            Log.d(TAG, "login to " + url.toString());
             try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
                 dos.writeBytes(physioKey);
             }catch (IOException e){
@@ -46,10 +50,10 @@ public class HttpClient
 
             try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 key = br.readLine();
-                    System.out.println("key" + key);
+                Log.d(TAG, "got login key" + key);
                 return key;
             }catch (IOException e){
-                Log.d(TAG, "ISR"+ e);
+                Log.e(TAG, "ISR"+ e);
             }
         }catch (IOException e){
             e.printStackTrace();
@@ -58,13 +62,14 @@ public class HttpClient
         return null;
     }
 
-    public void dataPost(String text){
+    public void dataPost(Object data){
+        Log.d(TAG, "logging data ...\n" + data.toString());
 
         new Thread(new Runnable() {
             @Override
             public void run() {
 
-                if (key.length() > 0) {
+                if (key.length() <= 0) {
                     login();
                 }
 
@@ -75,30 +80,63 @@ public class HttpClient
                     connection = url.openConnection();
                     connection.setDoOutput(true);
 
-                    String test = "{'key': '" + key + "','data':'" + text + "'}";
+                    LoggingData loggingData = new LoggingData(key);
+                    loggingData.setData(data);
+                    String stringData = loggingData.getRoot().toString();
 
                     connection.setRequestProperty("Content-Type", "text/plain");
-                    connection.setRequestProperty("Content-Length", Integer.toString(test.length()));
+                    connection.setRequestProperty("Content-Length", Integer.toString( stringData.length() ));
 
                     try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
-                        dos.writeBytes(test);
-                        System.out.println(test);
+                        dos.writeBytes(stringData);
+                        Log.d(TAG, "data send");
                     } catch (IOException e) {
                         Log.e(TAG, "DOS" + e);
                     }
 
                     try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                         String line = br.readLine();
-                        System.out.println(line);
+                        Log.d(TAG, "received response: " + line);
+
+                        if (!line.contains("OK")){
+                            Log.e(TAG, "data not send!");
+                            key = "";       // reset key
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 } catch (NullPointerException | IOException e) {
-                    Log.e(TAG, "sending Data" + e);
+                    Log.e(TAG, "error sending Data" + e);
                 }
 
             }
         }).start();
 
+    }
+
+    private class LoggingData{
+        private JSONObject root = new JSONObject();
+
+        public LoggingData(String sessionKey){
+            try {
+                root.put("key", sessionKey);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Boolean setData(Object data){
+            try {
+                root.put("data", data);
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        public JSONObject getRoot() {
+            return root;
+        }
     }
 }
