ab7deb4bf819b4ca88ec47b6f2031b778a28a3c5
Hannes Eilers <mail@hanneseilers.de>
2022-06-16 16:20:37 +0200

    Fixed error, if no key was set due to no network connection.

--- app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
+++ app/src/main/java/de/fhkiel/srcms/apps/therapy/physical/p/workout/HttpClient.java
@@ -69,7 +69,7 @@ public class HttpClient
             @Override
             public void run() {
 
-                if (key && key.length() <= 0) {
+                if (key == null || key.length() <= 0) {
                     login();
                 }
 
